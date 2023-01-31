390ed5360cf4c6c0f8854df81f4c2a4107cc04c6
Lucas Slade <lucas.sheldon.slade@gmail.com>
2023-01-31 09:55:02 -0700

    done done

---
+++ src/main/java/com/codeup/blogpost/controllers/UserController.java
@@ -0,0 +1,39 @@
+package com.codeup.blogpost.controllers;
+
+
+
+import com.codeup.blogpost.model.User;
+import com.codeup.blogpost.repositories.UsersRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+    private UsersRepository users;
+    private PasswordEncoder passwordEncoder;
+
+    public UserController(UsersRepository users, PasswordEncoder passwordEncoder){
+        this.users = users;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/sign-up")
+    public String showRegisterForm(Model model){
+        model.addAttribute("user", new User());
+        return "posts/register";
+
+    }
+
+    @PostMapping("/sign-up")
+    public String addUser(@ModelAttribute User user){
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        users.save(user);
+        return "redirect:/login";
+    }
+
+}
